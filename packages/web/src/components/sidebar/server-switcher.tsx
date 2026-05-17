100175169c88315a097754633973c60b2e7d2baf
hunmer <liaoyanjie2000@gmail.com>
2026-05-17 10:54:23 +0800

    fix

--- packages/web/src/components/sidebar/server-switcher.tsx
+++ packages/web/src/components/sidebar/server-switcher.tsx
@@ -45,7 +45,7 @@ export function ServerSwitcher() {
     setActiveId(server.id);
     saveActiveId(server.id);
     setActiveServerCookie(server.url);
-    window.location.reload();
+    window.location.href = "/";
   };
 
   const removeServer = (id: string) => {
