2a1a9f544adffd6d8a7078ee742de61f4c2dacac
hunmer <liaoyanjie2000@gmail.com>
2026-06-15 21:59:41 +0800

    fix

--- packages/server/src/services/execution-manager.ts
+++ packages/server/src/services/execution-manager.ts
@@ -528,7 +528,7 @@ export class ExecutionManager {
         : result;
 
       session.context[node.id] = step.output;
-      this.setNodeExecutionData(session, node.id, result);
+      this.setNodeExecutionData(session, node.id, normalizeNodeResult(result));
       if (node.type === 'start') this.setNodeExecutionInput(session, node.id, result);
 
       if (node.type === 'switch' && result?.__branch__) {
